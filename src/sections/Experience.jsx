c49dbd86bd46cc15c389c9bd6718f2e959cb6087
akhilworks2001 <112404416+akhilworks2001@users.noreply.github.com>
2025-03-19 19:01:44 +0530

    experience section

---
+++ src/sections/Experience.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <section className="c-space my-20" id="work">
+      <div className="w-full text-white-600">
+        <p className="head-text">My Work Experience</p>
+
+        <div className="work-container">
+          <div className="work-canvas">
+            <canvas>
+
+            </canvas>
+          </div>
+
+          <div className="work-content">
+            <div className="sm:py-10 py-5 sm:px-5 px-2.5">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Experience
